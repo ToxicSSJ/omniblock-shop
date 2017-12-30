d11fc8f71b2b84f5fa5292c76039a189c4209b84
Dell <Dell@192.168.1.72>
2017-12-30 09:18:52 -0600

    SHOP: 2DEV-2.5.7(Filtros en la creación de un ShopSign)
    -Se añadió un permiso nuevo (ShopSign.admin)

--- src/net/omniblock/shop/api/ShopSignManager.java
+++ src/net/omniblock/shop/api/ShopSignManager.java
@@ -15,6 +15,7 @@ import net.omniblock.shop.ShopPlugin;
 import net.omniblock.shop.api.config.LineRegex;
 import net.omniblock.shop.api.object.AbstractShop;
 import net.omniblock.shop.api.object.AdminShop;
+import net.omniblock.shop.api.object.UserShop;
 
 public class ShopSignManager {
 
@@ -82,17 +83,74 @@ public class ShopSignManager {
 			
 			if(e.getLines().length >= 2) {
 				
-				if(e.getLine(0).equalsIgnoreCase(LineRegex.CREATE_ADMIN_SHOP_UP) && e.getLine(1).equalsIgnoreCase(LineRegex.CREATE_BUY_SHOP_MIDDLE)) {
+				/* El cartel esta pensado que funcione de la sig manera:
+				 * 
+				 * [TIENDA / ADMIN]
+				 * [COMPRAR / VENDER]
+				 * [ITEM]				//Se puede dejar en blanco (item en mano)
+				 * [PRECIO x CANTIDAD]	//Si es admin y esta en blanco, poner solo la cantidad, el precio se pone automaticamente.
+				 * 
+				 */
+
+				//Para carteles ADMIN;
+				if(e.getPlayer().hasPermission("ShopSign.admin") && e.getLine(0).equalsIgnoreCase(LineRegex.CREATE_ADMIN_SHOP_UP)){
 					
-					AdminShop shop = new AdminShop(
-							(Sign) e.getBlock().getState(),
-							Resolver.getNetworkIDByName(e.getPlayer().getName()),
-							UUID.randomUUID().toString().substring(0, 4));
-					
-					shop.saveSign();
-					addShop(shop);
-					return;
-					
+					//ADMINISTRADOR QUE COMPRA
+					if(e.getLine(1).equalsIgnoreCase(LineRegex.CREATE_BUY_SHOP_MIDDLE)) {
+						
+						AdminShop shop = new AdminShop(
+								(Sign) e.getBlock().getState(),
+								Resolver.getNetworkIDByName(e.getPlayer().getName()),
+								UUID.randomUUID().toString().substring(0, 4));
+						
+						shop.saveSign();
+						addShop(shop);
+						return;
+						
+					}
+					//ADMINISTRADOR QUE VENDE
+					else if(e.getLine(1).equalsIgnoreCase(LineRegex.CREATE_SELL_SHOP_MIDDLE)) {
+						
+						AdminShop shop = new AdminShop(
+								(Sign) e.getBlock().getState(),
+								Resolver.getNetworkIDByName(e.getPlayer().getName()),
+								UUID.randomUUID().toString().substring(0, 4));
+						
+						shop.saveSign();
+						addShop(shop);
+						return;
+						
+					}
+				}
+				
+				//Para carteles TIENDA;
+				if(e.getLine(0).equalsIgnoreCase(LineRegex.CREATE_USER_SHOP_UP)){
+					//USER QUE COMPRA
+					if(e.getLine(1).equalsIgnoreCase(LineRegex.CREATE_BUY_SHOP_MIDDLE)) {
+						
+						UserShop shop = new UserShop(
+								(Sign) e.getBlock().getState(),
+								Resolver.getNetworkIDByName(e.getPlayer().getName()),
+								UUID.randomUUID().toString().substring(0, 4));
+						
+						shop.saveSign();
+						addShop(shop);
+						return;
+						
+					}
+					//USER QUE VENDE
+					else if(e.getLine(1).equalsIgnoreCase(LineRegex.CREATE_SELL_SHOP_MIDDLE)) {
+						
+						UserShop shop = new UserShop(
+								(Sign) e.getBlock().getState(),
+								Resolver.getNetworkIDByName(e.getPlayer().getName()),
+								UUID.randomUUID().toString().substring(0, 4));
+						
+						shop.saveSign();
+						addShop(shop);
+						return;
+						
+					}
 				}
 				
 			}
